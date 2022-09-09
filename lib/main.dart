6a900d4a8080f240b4beff4cf8742f818afd4ef6
Ria Jindal <riajindal24.rj@gmail.com>
2022-09-09 18:53:40 +0530

    magic 8 ball project completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text("Ask Me Anything"),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: TextButton(
+          onPressed: (){
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+              print(ballNumber);
+            });
+          },
+          child: Image.asset("images/ball$ballNumber.png"),
+        ),
+      ),
+    );
+  }
+}
